464b9bc6730324d9b0733cc9206320b30cd69ee8
Manu <manuelchavarin@aori.mx>
2018-07-02 18:17:07 -0500

    pipes numero y porcentaje

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  nombre:string = "Fernando"
+  arreglo:number[] = [1,2,3,4,5,6,7,8,9,10]
+  PI:number = Math.PI;
 }
